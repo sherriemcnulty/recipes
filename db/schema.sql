053e16506d1b8867f9bc0c0a8d48f562c57808c4
Sherrie <sherriemcnulty@gmail.com>
2020-01-13 13:40:45 -0500

    Adding database schema & seeds

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE recipes_db;
+USE recipesDB;
+
+CREATE TABLE recipes
+(
+    id INT NOT NULL
+    AUTO_INCREMENT,
+	photo LONGBLOB NULL,
+	title VARCHAR
+    (100) NOT NULL,
+	prep_time INT NOT NULL,
+    cook_time INT NOT NULL,
+    servings INT NOT NULL,
+	ingredients VARCHAR
+    (255) NOT NULL,
+	directions LONGTEXT NOT NULL,
+    PRIMARY KEY
+    (id)
+);
+
+    -- The client table will be used when login authentication is implemented
+    CREATE TABLE clients
+    (
+        id int NOT NULL
+        AUTO_INCREMENT,
+	client VARCHAR
+        (50) NOT NULL,
+    pwd  CHAR
+        (60),
+	PRIMARY KEY
+        (id)
+);
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE recipes_db;
+
+INSERT INTO recipes
+    (title, prep_time, cook_time, servings, ingredients, directions)
+VALUES
+    ("Sugar Cookies", 15, 20, 12, "2 3/4 cups all-purpose flour, 1 teaspoon baking soda, 1 cup butter (softened), 1 egg, 1 teaspoon vanilla extract", "Preheat oven to 375◦ F. (2) Stir together flour, baking soda and baking powder. Set aside., (3) Bake 8 to 10 minutes in the preheated oven, or until golden. Let stand on cookie sheet two minutes before removing to cool on wire racks.");
